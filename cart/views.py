7a3f77d63c82c71cadf20d75626191b893a51d4d
Chirag Chawla <71888352+chiirag2611@users.noreply.github.com>
2025-01-20 19:06:59 +0100

    debugged add to cart increment

--- cart/views.py
+++ cart/views.py
@@ -2,54 +2,64 @@ from django.shortcuts import render, redirect
 from store.models import Product
 from .models import Cart, CartItem
 
-from django.http import HttpResponse
-
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
         cart = request.session.create()
     return cart
 
-
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
-        cart= Cart.objects.get(cart_id=_cart_id(request))
+        cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
-        cart = Cart.objects.create(
-            cart_id = _cart_id(request)
-        )
+        cart = Cart.objects.create(cart_id=_cart_id(request))
     cart.save()
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
         cart_item.quantity += 1
+        cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(
-            product = product,
-            quantity = 1,
-            cart = cart,
+            product=product,
+            quantity=1,
+            cart=cart,
         )
-        cart_item.save()
-    
     return redirect('cart')
 
+def update_cart(request, product_id, action):
+    product = Product.objects.get(id=product_id)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    if action == 'increase':
+        cart_item.quantity += 1
+    elif action == 'decrease' and cart_item.quantity > 1:
+        cart_item.quantity -= 1
+    cart_item.save()
+
+    return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try: 
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
-            total += (cart_item.product.price * cart_item.quantity)
+            total += cart_item.sub_total()  # Use the sub_total method to calculate
             quantity += cart_item.quantity
-
-    except ObjectDoesNotExist:
+        tax = (1.35 * total/100)
+        grand_total = total + tax
+    except Cart.DoesNotExist:
         pass
 
     context = {
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
+        'tax'       : tax,
+        'grand_total': grand_total
     }
 
     return render(request, 'store/cart.html', context)
