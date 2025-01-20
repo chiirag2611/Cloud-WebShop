c059dbfbc94472ff3b629c8be051718607b8c8ca
Chirag Chawla <71888352+chiirag2611@users.noreply.github.com>
2025-01-20 19:24:47 +0100

    implemented decrement button

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404 
 from store.models import Product
 from .models import Cart, CartItem
 
@@ -41,6 +41,21 @@ def update_cart(request, product_id, action):
     cart_item.save()
 
     return redirect('cart')
+
+
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try: 
